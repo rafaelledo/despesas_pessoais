6533fe72b1e1fbf14da2598ed31c7a335cfb3a0c
Rafael Ledo <rafaeloliveiraledo@gmail.com>
2022-10-31 18:40:50 -0300

    remove MyHomePage class from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,18 @@
-import 'package:despesas_pessoais/model/transaction.dart';
 import 'package:flutter/material.dart';
+import './view/myhomepage.dart';
 
 void main(List<String> args) {
-  runApp(const ExpensesApp());
+	runApp(const ExpensesApp());
 }
 
 class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({super.key});
+	const ExpensesApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo tênis de corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-  ];
-
-  MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const SizedBox(
-          width: double.infinity,
-          child: Text(
-            'Despesas Pessoais',
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          const SizedBox(
-            child: Card(
-              color: Colors.blue,
-              elevation: 5,
-              child: Text(
-                'Gráfico',
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-          Column(
-            children: _transactions.map((tr) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      )),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        tr.value.toString(),
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tr.title,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(tr.date.toString(),
-                            style: const TextStyle(
-                              color: Colors.grey,
-                            )),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          )
-        ],
-      ),
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+		return MaterialApp(
+			debugShowCheckedModeBanner: false,
+			home: MyHomePage(),
+		);
+	}
 }
--- lib/model/transaction.dart
+++ lib/model/transaction.dart
@@ -1,13 +1,13 @@
 class Transaction {
-  final String id;
-  final String title;
-  final double value;
-  final DateTime date;
+	final String id;
+	final String title;
+	final double value;
+	final DateTime date;
 
-  Transaction({
-    required this.id,
-    required this.title,
-    required this.value,
-    required this.date,
-  });
+	Transaction({
+		required this.id,
+		required this.title,
+		required this.value,
+		required this.date,
+	});
 }
